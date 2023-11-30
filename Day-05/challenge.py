d0dc68e67271be107747f23fbb1a7e48a5845142
Edoghotu Gideon Azibaobuom <gideon.edoghotu@cyphercrescent.com>
2023-11-30 14:27:54 +0100

    day 5 completed(60%)

---
+++ Day-05/challenge.py
@@ -0,0 +1,13 @@
+print("Welcome to the PyPassword Generator!")
+
+no_of_letters = input("How many letters would you like in your password?\n")
+no_of_symbols = input("How many symbols would you like?\n")
+no_of_nos = input("How many numbers would you like?\n")
+
+
+
+
+
+
+
+print(f"Here is your password: ")
