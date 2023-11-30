1706454257c66e792ee9d9ccf67d4d049dd15b82
Edoghotu Gideon Azibaobuom <gideon.edoghotu@cyphercrescent.com>
2023-11-30 10:32:09 +0100

    day 4 implemented

--- Day-04/challenge.py
+++ Day-04/challenge.py
@@ -32,26 +32,25 @@ length_of_store = len(store)
 
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 
-if (user_choice > length_of_store):
-    print("Out of range")
-
-print(store[user_choice])
-
-computer_choice = random.randint(0, length_of_store)
-print(f"Computer choose:\n{store[computer_choice]}")
-
 if user_choice >= 3 or user_choice < 0:
-  print("You typed an invalid number, you lose!") 
-elif user_choice == 0 and computer_choice == 2:
-  print("You win!")
-elif computer_choice == 0 and user_choice == 2:
-  print("You lose")
-elif computer_choice > user_choice:
-  print("You lose")
-elif user_choice > computer_choice:
-  print("You win!")
-elif computer_choice == user_choice:
-  print("It's a draw")
+  print("You typed an invalid number, you lose!")
+else:
+    print(store[user_choice])
+
+    computer_choice = random.randint(0, length_of_store)
+    print(f"Computer choose:\n{store[computer_choice]}")
+
+
+    if user_choice == 0 and computer_choice == 2:
+      print("You win!")
+    elif computer_choice == 0 and user_choice == 2:
+      print("You lose")
+    elif computer_choice > user_choice:
+      print("You lose")
+    elif user_choice > computer_choice:
+      print("You win!")
+    elif computer_choice == user_choice:
+      print("It's a draw")
 
 
 ####### Debugging challenge: #########
