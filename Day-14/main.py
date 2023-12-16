0c8d29537c0e5ee08248f98926775d62d854e75a
giddy11 <edoghotugiddy@gmail.com>
2023-12-16 02:56:40 +0100

    updated

--- Day-14/main.py
+++ Day-14/main.py
@@ -3,16 +3,17 @@ from art import logo, vs
 from game_data import data
 import os
 
-
 is_answer_correct = True
 compareA = 0
 compareB = 0
 current_score = 0
 
-def clear(): 
-    os.system('clear')
 
-def GetBothCompareValues():
+def clear():
+    os.system('cls')
+
+
+def get_both_compare_values():
     global compareA, compareB
     while compareA == compareB:
         compareA = randint(0, 49)
@@ -21,23 +22,25 @@ def GetBothCompareValues():
         # compareA = 3
         # compareB = 4
 
-def CheckFollowers():
+
+def check_followers():
     global compareA, compareB
     if data[compareA]["follower_count"] >= data[compareB]["follower_count"]:
         return "a"
     else:
-        return "b" 
+        return "b"
+
 
 while is_answer_correct:
     print(logo)
-    GetBothCompareValues()
+    get_both_compare_values()
     print(f"Compare A: {data[compareA]["name"]}, a {data[compareA]["description"]}, from {data[compareA]["country"]}.")
     print(vs)
     print(f"Compare B: {data[compareB]["name"]}, a {data[compareB]["description"]}, from {data[compareB]["country"]}.")
 
     compareOptions = input("Who has more followers? Type 'A' or 'B': ").lower()
 
-    if compareOptions == CheckFollowers():
+    if compareOptions == check_followers():
         current_score += 1
         clear()
         print(f"You're right! Current Score: {current_score}")
@@ -46,7 +49,3 @@ while is_answer_correct:
         print(logo)
         is_answer_correct = False
         print(f"Sorry, that's wrong. Final Score: {current_score}")
-
-    
-
-
