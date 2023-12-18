ed167eabb45daa655899c34ff75413c5c8d57609
giddy11 <edoghotugiddy@gmail.com>
2023-12-18 12:58:21 +0100

    day 16 completed(100%) refactored

--- Day-16/main.py
+++ Day-16/main.py
@@ -2,27 +2,22 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+turn_on = True
+coffee_maker = CoffeeMaker()
+menu = Menu()
+money_machine = MoneyMachine()
+menu_item = MenuItem("nutri", 50, 0, 30, 1.5)
 
-andrew = CoffeeMaker()
-order = Menu()
-my_cash = MoneyMachine()
-menu = MenuItem("nutri", 50, 0, 30, 1.5)
+all_items_available = menu.get_items()
 
-"""Check if te drink is available"""
-latte = order.find_drink("latte")
-all_items_available = order.get_items()
-print(all_items_available)
-# print(latte)
-
-"""Check for the cost of each item"""
-andrew.report()
-
-"""check if latte drink is sufficient"""
-is_latte_sufficient = andrew.is_resource_sufficient(latte)
-# print(is_latte_sufficient)
-
-
-# my_cash.report()
-# my_cash.make_payment(latte.cost)
-# andrew.make_coffee(latte)
-# andrew.report()
+while turn_on:
+    choice = input(f"What would you like? {all_items_available}: ").lower()
+    if choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    elif choice == "off":
+        turn_on = False
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
