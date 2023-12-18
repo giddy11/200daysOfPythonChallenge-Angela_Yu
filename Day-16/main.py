bc0f0d4b14bce47bf89fdf6b7e133b0f35ec0291
giddy11 <edoghotugiddy@gmail.com>
2023-12-18 10:09:10 +0100

    day 16 completed(100%)

--- Day-16/main.py
+++ Day-16/main.py
@@ -5,8 +5,24 @@ from money_machine import MoneyMachine
 
 andrew = CoffeeMaker()
 order = Menu()
+my_cash = MoneyMachine()
+menu = MenuItem("nutri", 50, 0, 30, 1.5)
 
-order.find_drink("latte")
+"""Check if te drink is available"""
+latte = order.find_drink("latte")
+all_items_available = order.get_items()
+print(all_items_available)
+# print(latte)
 
+"""Check for the cost of each item"""
 andrew.report()
-andrew.make_coffee(order)
+
+"""check if latte drink is sufficient"""
+is_latte_sufficient = andrew.is_resource_sufficient(latte)
+# print(is_latte_sufficient)
+
+
+# my_cash.report()
+# my_cash.make_payment(latte.cost)
+# andrew.make_coffee(latte)
+# andrew.report()
